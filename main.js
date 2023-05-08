4a6c237b37026647c74969a4cd1b711538024ca4
AlexanderApollGray <90724200+AlexanderApollGray@users.noreply.github.com>
2023-05-08 11:22:32 -0600

    work

---
+++ main.js
@@ -0,0 +1,11 @@
+// Soccer Goalie Game JavaScript by Xander
+
+// Setup canvas
+let cnv = document.getElementById("myCanvas");
+let ctx = cnv.getContext("2d");
+cnv.width = 800;
+cnv.height = 650;
+
+ctx.font = "70px Arial, Helvetica, sans-serif";
+ctx.fillStyle = "#8B008B";
+ctx.fillText("Play", 320, 310);
