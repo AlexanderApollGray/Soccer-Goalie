01a845483193b3e3521e207249bf346e3dce1a24
AlexanderApollGray <90724200+AlexanderApollGray@users.noreply.github.com>
2023-05-09 11:21:01 -0600

    work

--- main.js
+++ main.js
@@ -9,3 +9,53 @@ cnv.height = 650;
 ctx.font = "70px Arial, Helvetica, sans-serif";
 ctx.fillStyle = "#8B008B";
 ctx.fillText("Play", 320, 310);
+
+// Global Variables
+let mouseIsPressed = false;
+let upIsPressed = false;
+let spaceIsPressed = false;
+let goalieX = 400;
+let goalieY = 300;
+
+// Event Listeners
+document.addEventListener("mousedown", mousedownHandler);
+document.addEventListener("mouseup", mouseupHandler);
+document.addEventListener('keydown', keydownHandler);
+document.addEventListener('keyup', keyupHandler);
+
+function mousedownHandler() {
+    mouseIsPressed = true;
+}
+
+function mouseupHandler() {
+    mouseIsPressed = false;
+}
+
+function keydownHandler(event) {
+    if (event.code === "ArrowUp") {
+        upIsPressed = true;
+    } if (event.code === "Space") {
+        spaceIsPressed = true;
+    }
+}
+
+function keyupHandler(event) {
+    if (event.code === "ArrowUp") {
+        upIsPressed = false;
+    } if (event.code === "Space") {
+        spaceIsPressed = false;
+    }
+}
+
+// Main Program
+requestAnimationFrame(start);
+
+function start() {
+    if (mouseIsPressed) {
+        ctx.drawImage(document.getElementById('goalie-default'), goalieX, goalieY, 200, 200);
+    }
+    if (upIsPressed) {
+        goalieX += 5;
+    }
+    requestAnimationFrame(start);
+}
